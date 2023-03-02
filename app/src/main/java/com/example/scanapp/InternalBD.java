36fcd74fc230383858ed5bea7b382f0d179d414d
Manuel Torres <manuel.torres.arg@gmail.com>
2023-03-02 07:05:27 -0300

    Agregada activity bd local

---
+++ app/src/main/java/com/example/scanapp/InternalBD.java
@@ -0,0 +1,17 @@
+package com.example.scanapp;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InternalBD extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+}
